df2b16d8d394766ec3130107ad3ff6044aedc707
木村直靖 <>
2019-10-04 00:11:47 +0900

    カーネル修正

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends ConsoleKernel
       $schedule->command('command:usercommand')->daily();//1日に一度、ユーザー情報を更新
       $schedule->command('command:coincommand')->daily();//1日に一度、coinの取引高を更新
       $schedule->command('command:coinhourcommand')->hourly();//1時間に一度、1時間のツイート数を更新
-      $schedule->command('command:coindaycommand')->->cron('10 0 * * *');//1日のツイート数を更新
+      $schedule->command('command:coindaycommand')->cron('15 0 * * *');//1日のツイート数を更新
       $schedule->command('command:coinweekcommand')->daily();//1日に一度、1週間のツイート数を更新
     }
         // $schedule->command('inspire')
