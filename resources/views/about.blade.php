52559613129f4de1436c376fddf480086487aad9
木村直靖 <>
2019-10-06 14:57:16 +0900

    ワンモア

---
+++ resources/views/about.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+
+<section class="p-decs__main">
+  <div class="p-desc__container">
+
+    <h2 class="p-desc__title c-text">
+      本サービスについて
+    </h2>
+    <p class="p-desc__text c-text">
+      仮想通貨取引が始まり数年。<br>
+      取引可能な通貨の種類は多く、ユーザーの各通貨への興味の推移や関連ニュースは非常に多種多様です。<br>
+      通貨の取引にあたり、こういった情報を抑える作業は欠かせません。<br>
+      『CryptoTrend』は、そんな情報収集のお手伝いをするサービスです。<br>
+      <br>
+    </p>
+  </div>
+</section>
+
+
+@endsection
